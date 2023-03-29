f265f6fd3e39461a5cbb50aec6853c5297166212
SurjitRedsky <surjitsingh.redskyatech@gmail.com>
2023-03-29 18:37:20 +0530

    login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./Login.css";
-import img from '../Images/instagramAdd.png'
-
+import img from "../Images/instagramAdd.png";
+import Footer from "../Components/Footer";
+import LoginForm from "../Components/LoginForm";
 
 const Login = () => {
   return (
-    <section className="section">
-      <div className="main">
-        <article className="loginPage">
-          <div className="ImageContainer">
-
-            <img src={img} ></img>
+    <>
+      <section className="section">
+        <div className="loginPage">
+          <div className="main">
+            <div className="imageContainer">
+              <img src=""></img>
+            </div>
+            <div className="loginContainer">
+              <LoginForm />
+            </div>
           </div>
-          <div className="loginContainer">login</div>
-        </article>
-      </div>
-
-      <footer>footer</footer>
-    </section>
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 };
 export default Login;
